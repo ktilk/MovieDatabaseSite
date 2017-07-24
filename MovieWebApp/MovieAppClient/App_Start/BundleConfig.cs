5aa3c08c662b6bda147f0a2e05f2f5d9aa89ce47
Kaspar Tilk <kaspar.tilk@itcollege.ee>
2017-07-24 10:51:23 +0300

    Added script and style bundles

---
+++ MovieWebApp/MovieAppClient/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Optimization;
+
+namespace MovieAppClient
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                        "~/Scripts/angular.js",
+                        "~/Scripts/angular-route.js",
+                        "~/Scripts/angular-animate.js",
+                        "~/Scripts/angular-touch.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/App/app.js",
+                        "~/App/data.js",
+                        "~/App/Controllers/movieController.js",
+                        "~/App/Services/moviesService.js",
+                        "~/App/Filters/movieFilter.js",
+                        "~/Scripts/angucomplete.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/foundation6").Include(
+                      "~/Assets/js/vendor/jquery.js",
+                      "~/Assets/js/vendor/what-input.js",
+                      "~/Assets/js/vendor/foundation.js",
+                      "~/Assets/js/app.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Assets/css/foundation.css",
+                      "~/Assets/css/app.css"));
+        }
+    }
+}
---
+++ MovieWebApp/MovieAppClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieAppClient.Global" Language="C#" %>
